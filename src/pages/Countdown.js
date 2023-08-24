c1e09a67c656e982c78284793d0ed127f05c7d65
enkhturBaina <eegii.111.gg@gmail.com>
2023-08-24 18:02:36 +0800

    zaswar

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import "../countdownScript";
 import "../countdownCss.css";
 import ProgressBar from "@ramonak/react-progress-bar";
 import moment from "moment";
 
 function CountDown() {
-  var startOfDate = moment("01-22-2023", "MM-DD-YYYY");
-  var endDate = moment("09-17-2023", "MM-DD-YYYY");
+  const [percNumber, setPercNumber] = useState(null);
+  var startOfDate = moment("01-22-2023 09:00:00", "MM-DD-YYYY HH:mm:ss");
+  var endDate = moment("09-17-2023 05:00:00", "MM-DD-YYYY HH:mm:ss");
   var todayDate = moment();
 
   const daysDifference = moment(endDate).diff(startOfDate, "days");
@@ -14,6 +15,31 @@ function CountDown() {
   const difference = todayDate.diff(startOfDate, "days");
 
   const resultPerc = Math.round((difference / daysDifference) * 100);
+  function getDatePercent() {
+    let dateInQuestion = new Date(Date.now());
+
+    //we are copying the value of the date object into a new object:
+    let startOfDay = new Date(dateInQuestion.valueOf());
+
+    //define the beginning of the day. Depending on time zone and browser, this may need tweaking:
+    startOfDay.setHours(0);
+    startOfDay.setMinutes(0);
+    startOfDay.setSeconds(0);
+    startOfDay.setMilliseconds(0);
+
+    let lengthOfDay = 1000 * 60 * 60 * 24; //ms in a day
+
+    //subtract to find time since beginning of the day, divide by
+    //number of ms in day, and then multiply by 100 to get percentage
+    return Math.round(
+      ((dateInQuestion.valueOf() - startOfDay.valueOf()) / lengthOfDay) * 10000
+    );
+  }
+  setInterval(() => {
+    setPercNumber(getDatePercent());
+    console.log("percNumber", percNumber);
+  }, 1000);
+
   return (
     <div
       style={{
@@ -36,6 +62,9 @@ function CountDown() {
         <div id="timer"></div>
         <h2>😍 Бидний уулзах өдөр 😍</h2>
         <h2>🕧(2023/01/22 - 2023/09/17)🕧</h2>
+        <h1
+          style={{ color: "red", textAlign: "center" }}
+        >{`${resultPerc}.${percNumber} %`}</h1>
       </div>
       <div style={{ width: 300 }}>
         <ProgressBar
