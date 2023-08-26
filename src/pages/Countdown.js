31271cf55130e09da691050942303a81daa14099
enkhturBaina <eegii.111.gg@gmail.com>
2023-08-26 15:17:58 +0800

    zaswar

--- src/pages/Countdown.js
+++ src/pages/Countdown.js
@@ -15,31 +15,7 @@ function CountDown() {
   const difference = todayDate.diff(startOfDate, "days");
 
   const resultPerc = Math.round((difference / daysDifference) * 100);
-  function getDatePercent() {
-    let dateInQuestion = new Date(Date.now());
-
-    //we are copying the value of the date object into a new object:
-    let startOfDay = new Date(dateInQuestion.valueOf());
-
-    //define the beginning of the day. Depending on time zone and browser, this may need tweaking:
-    startOfDay.setHours(0);
-    startOfDay.setMinutes(0);
-    startOfDay.setSeconds(0);
-    startOfDay.setMilliseconds(0);
-
-    let lengthOfDay = 1000 * 60 * 60 * 24; //ms in a day
-
-    //subtract to find time since beginning of the day, divide by
-    //number of ms in day, and then multiply by 100 to get percentage
-    return Math.round(
-      ((dateInQuestion.valueOf() - startOfDay.valueOf()) / lengthOfDay) * 10000
-    );
-  }
-  setInterval(() => {
-    setPercNumber(getDatePercent());
-    console.log("percNumber", percNumber);
-  }, 1000);
-
+  w;
   return (
     <div
       style={{
@@ -62,9 +38,6 @@ function CountDown() {
         <div id="timer"></div>
         <h2>😍 Бидний уулзах өдөр 😍</h2>
         <h2>🕧(2023/01/22 - 2023/09/17)🕧</h2>
-        <h1
-          style={{ color: "red", textAlign: "center" }}
-        >{`${resultPerc}.${percNumber} %`}</h1>
       </div>
       <div style={{ width: 300 }}>
         <ProgressBar
